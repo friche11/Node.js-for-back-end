c33c7779135ec8b32536e1b97291ba5148d13683
friche11 <vicfriche@gmail.com>
2023-03-30 13:28:38 -0300

    Servidor criado e rodando

--- Conceitos iniciais/app.js
+++ Conceitos iniciais/app.js
@@ -1,3 +1,3 @@
 var http = require('http');   //modulo http do Node.js
 
-http.createServer().listen(8081)  //abrir servidor para haver comunicacao com cliente. Funcao listen identifica qual porta de rede sera aberto o servidor
+http.createServer().listen(8080)  //abrir servidor para haver comunicacao com cliente. Funcao listen identifica qual porta de rede sera aberto o servidor
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express"); //Importa o pacote Express para utilizar as funcoes e recursos do Express
+
+const app = express(); //Funcao cria uma instancia do framework Express pra variavel app.
+
+
+
+
+
+app.listen(8081, function(){
+    console.log("Servidor rodando na url http://localhost:8081")
+});  //Tem que ser a ultima linha do codigo para que tudo funcione dentro do servidor.
+
+
