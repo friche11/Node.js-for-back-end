dcb9ee560cb8e4c9e162f4207676c0852df82dbe
friche11 <vicfriche@gmail.com>
2023-03-31 21:46:31 -0300

    Criando models por Sequelize

--- bd.js
+++ bd.js
@@ -9,3 +9,40 @@ sequelize.authenticate().then(function(){
 }).catch(function(erro){
         console.log("Falha ao se conectar: "+erro)
 })
+
+const Postagem = sequelize.define('postagens', {
+    titulo:{
+        type: Sequelize.STRING  //Caracteres limitados
+    },
+    conteudo:{
+        type: Sequelize.TEXT   //Caracteres ilimitados
+    }
+})  //Criar modulo de postagem
+
+
+Postagem.create({
+    titulo: "Titulo qualquer",
+    conteudo: "sauhhuashusahusahuusahu"
+})
+
+
+const Usuario = sequelize.define('usuarios',{
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+//Usuario.sync({force:true})
+
+//Postagem.sync({
+ //   force:true     //gerar a tabela do objeto postagem no banco de dados
+//})
