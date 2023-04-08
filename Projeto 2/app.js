0bb5638cbd3074e2781ad58d057c46c4e5ae7368
friche11 <vicfriche@gmail.com>
2023-04-07 21:41:59 -0300

    Adicao de bootstrap

--- Projeto 2/app.js
+++ Projeto 2/app.js
@@ -3,6 +3,8 @@
     const handlebars = require('express-handlebars')
     const bodyParser = require('body-parser')
     const app = express()
+    const admin = require('./routes/admin')
+    const path = require('path')
     //const mongoose = require('mongoose')
 
 // Configurações
@@ -12,7 +14,21 @@
     // Handlebars
         app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars');
+    // Public
+        app.use(express.static(path.join(__dirname + "/public")))
 // Rotas
+    app.get('/', (req,res) => {
+    res.send("Pagina principal")
+    })
+
+    app.get('/posts', (req,res) => {
+    res.send("Pagina de posts")
+    })
+
+    app.get('/categorias', (req,res) => {
+        res.send("Pagina de categorias")
+    })
+    app.use('/admin', admin)
 
 
 // Outros
