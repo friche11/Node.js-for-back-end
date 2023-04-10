9d6c3fc6be610693f7489c75f8cf8f743b420ca2
friche11 <vicfriche@gmail.com>
2023-04-10 10:00:34 -0300

    Deletar categorias

--- Projeto 2/routes/admin.js
+++ Projeto 2/routes/admin.js
@@ -106,4 +106,14 @@ router.post('/categorias/nova', (req,res) => {
 
 })
 
+router.post("/categorias/deletar", (req,res)=>{
+    Categoria.deleteOne({_id: req.body.id}).then(()=>{
+        req.flash("success_msg", "Categoria deletada com sucesso")
+        res.redirect("/admin/categorias")
+    }).catch((erro)=>{
+        req.flash("error_msg", "Houve um erro ao deletar a categoria")
+        res.redirect("/admin/categorias")
+    })
+})
+
 module.exports = router
