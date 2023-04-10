6a70ea13f9d1f8d2c5d5c9b597dc0ed2bc8a1cef
friche11 <vicfriche@gmail.com>
2023-04-10 08:19:16 -0300

    Validacao de formulario

--- Projeto 2/app.js
+++ Projeto 2/app.js
@@ -19,7 +19,7 @@
         app.use(flash())
     // Middleware
         app.use((req,res,next) =>{
-            res.locals.success_msg = req.flash("sucess_msg")
+            res.locals.success_msg = req.flash("success_msg")
             res.locals.error_msg = req.flash("error_msg")
             next()
         })
