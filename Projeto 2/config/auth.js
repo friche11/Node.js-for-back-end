8722e89510e061c8f8ccdc5f5c3cf809c38624aa
friche11 <vicfriche@gmail.com>
2023-04-13 23:09:21 -0300

    Autenticacao login

--- Projeto 2/config/auth.js
+++ Projeto 2/config/auth.js
@@ -8,7 +8,7 @@ const Usuario = mongoose.model("usuarios")
 
 module.exports = function(passport){
 
-    passport.use(new localStrategy({usernameField: 'email'}, (email,senha,done) =>{
+    passport.use(new localStrategy({usernameField: 'email', passwordField: 'senha'}, (email,senha,done) =>{
 
         Usuario.findOne({email:email}).then((usuario)=>{
             if(!usuario){
@@ -17,7 +17,7 @@ module.exports = function(passport){
 
             bcrypt.compare(senha, usuario.senha, (erro, batem) =>{
                 if(batem){
-                    return done(null, user)
+                    return done(null, usuario)
                 }else{
                     return done(null, false, {message: "Senha incorreta"})
                 }
@@ -31,10 +31,13 @@ module.exports = function(passport){
     done(null, usuario.id)
     })
 
-    passport.deserializeUser((id, done)=>{
-        User.findById(id,(err,usuario)=>{
-            done(err, user)
-        })
-    })
+    passport.deserializeUser(async (id, done) => {
+        try {
+          const usuario = await Usuario.findById(id);
+          done(null, usuario);
+        } catch (err) {
+          done(err, null);
+        }
+      })
 
 }
